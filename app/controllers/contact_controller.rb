9feddbd8fd195a5dc20e6a4bace89dc98291130a
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-04-29 14:22:06 -0300

    Add rspec

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,39 +1,43 @@
-# Create
-post '/contact' do
-  @contact = User.create(params[:contact])
+class Contact
+  def page
+    # Create
+    post '/contact' do
+      @contact = User.create(params[:contact])
 
-  redirect '/'
-end
+      redirect '/'
+    end
 
-# Read
-get '/contact/:id' do
-  @user = User.find(params[:id])
-  @title = "Information | Contact list"
-  @header = erb :header
+    # Read
+    get '/contact/:id' do
+      @user = User.find(params[:id])
+      @title = "Information | Contact list"
+      @header = erb :header
 
-  erb :contact
-end
+      erb :contact
+    end
 
-# Update
-get '/contact/:id/update' do
-  @user = User.find(params[:id])
-  @title = "Update | Contact list"
-  @header = erb :header
+    # Update
+    get '/contact/:id/update' do
+      @user = User.find(params[:id])
+      @title = "Update | Contact list"
+      @header = erb :header
 
-  erb :update_contact
-end
+      erb :update_contact
+    end
 
-# Update
-post '/contact/:id/update' do
-  User.update(params[:id], params[:contact])
+    # Update
+    post '/contact/:id/update' do
+      User.update(params[:id], params[:contact])
 
-  redirect "/contact/#{params[:id]}"
-end
+      redirect "/contact/#{params[:id]}"
+    end
 
-# Delete
-post '/contact/:id/delete' do
-  @user = User.find(params[:id])
-  @user.destroy
+    # Delete
+    post '/contact/:id/delete' do
+      @user = User.find(params[:id])
+      @user.destroy
 
-  redirect '/'
+      redirect '/'
+    end
+  end
 end
